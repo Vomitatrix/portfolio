aa6dab59977601a97fc8e9e83fa23e72688123f5
xubuntu-laptop <david.j.procop@gmail.com>
2023-01-09 19:49:26 -0500

    Updated project screenshots and added scroll progress bar to the top;

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.querySelectorAll('section').forEach(section => {
+    section.style.height = `${100 - getComputedStyle(document.querySelector('nav')).height}`;
+});
+
+window.addEventListener('scroll', function () {
+    document.getElementById('scroll-progress').style.width =
+        (100 * window.pageYOffset) / (this.document.body.scrollHeight - this.window.innerHeight) +
+        '%';
+});
